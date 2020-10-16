cf8d90ae3d18eaa88484e7e4c060c2d66b20dcc3
Ivan Dimitrov <ivan.dimitrov293@gmail.com>
2020-10-17 02:12:03 +0300

    [Update] Added helmet, compression and CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as compression from 'compression';
+import * as helmet from 'helmet';
 
 const bootstrap = async () => {
     const app = await NestFactory.create(AppModule);
+    app.use(compression());
+    app.use(helmet());
+    app.enableCors();
+
     await app.listen(8080);
 };
 
