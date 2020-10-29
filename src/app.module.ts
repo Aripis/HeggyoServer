2829ffd7ed4aefa3dcb695172ebe6b68b30977c9
Ivan Dimitrov <ivan.dimitrov293@gmail.com>
2020-10-29 23:16:47 +0200

    [Update] Updated and connected to CLoud SQL, configured TypeORM, added Enviroment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import ormconfig from './ormconfig';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true, load: [ormconfig] }),
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) =>
+                configService.get('database'),
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
---
+++ src/ormconfig.ts
@@ -0,0 +1,20 @@
+export default () => ({
+    database: {
+        type: process.env.DB_DRIVER,
+        host: process.env.DB_HOST,
+        extra: {
+            socketPath: process.env.DB_SOCKET,
+        },
+        port: parseInt(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        migrationsRun: false,
+        migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+        cli: {
+            migrationsDir: 'src/migrations',
+        },
+    },
+});
